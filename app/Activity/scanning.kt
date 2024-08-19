c807bcca788d78a4e01ee4123ec4fefc53c0a5a4
Paulxocmd <sibila.paul2@gmail.com>
2024-08-19 13:54:16 +0200

    updated scnaning.kt

--- app/Activity/scanning.kt
+++ app/Activity/scanning.kt
@@ -8,3 +8,17 @@ private val bluetoothAdapter by lazy{
     val bluetoothManager =getSystemService(Context.BLUETOOTH_SERVICE) as bluetoothManager
     bluetoothManager.adapter
 }
+//man braucht Scansettings, das ist jetzt eines der Simpleren:
+private val scanSettings = ScanSettings.Builder()
+    .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+    .build
+//hier die scancallback function
+
+private val scancallback = object :ScanCallback(){
+    override fun onScanResult(callbackType: Int,result: ScanResult){
+        with(result.deice) {
+            Log.i("ScanCallback","BLE Gerät gefunden! Name ${name ?: "Unnamed"}, address: $address")
+        }
+    }
+}
+//jetzt gehts an die start BLEscann function :)
